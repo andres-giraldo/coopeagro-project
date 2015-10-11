6c2181e2559061ea397d8694645b3d32d1c1c907
YEISSON <YEISSON@YEISSON-PC>
2015-10-11 18:23:30 -0500

    Creacion, Modificacion, Eliminacion listos - Producto!!!!!

--- coopeagro-web/src/main/java/org/coopeagro/beans/BeanProducto.java
+++ coopeagro-web/src/main/java/org/coopeagro/beans/BeanProducto.java
@@ -8,12 +8,16 @@ package org.coopeagro.beans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.inject.Named;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
 import org.coopeagro.controladores.ProductoJpaController;
+import org.coopeagro.controladores.exceptions.NonexistentEntityException;
 import org.coopeagro.entidades.Producto;
 
 /**
@@ -31,11 +35,54 @@ public class BeanProducto {
      * Creates a new instance of BeanProducto
      */
     public BeanProducto() {
-        ProductoJpaController controller = (ProductoJpaController)servletContext.getAttribute("productoJpaController");
+        //ProductoJpaController controller = (ProductoJpaController)servletContext.getAttribute("productoJpaController");
+        productos = listarProductos();
     }
     
     public void guardar(){
-        
+        FacesMessage msg = null;
+        try {
+            ProductoJpaController controller = (ProductoJpaController) servletContext.getAttribute("productoJpaController");
+            controller.create(producto);
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "El registro fue insertado con éxito");
+        } catch (Exception ex) {
+            Logger.getLogger(BeanProducto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        productos = listarProductos();
+    }
+    
+    public String eliminar(Integer id){
+        try {
+            ProductoJpaController controller = (ProductoJpaController) servletContext.getAttribute("productoJpaController");
+            controller.destroy(id);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(BeanProducto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        productos = listarProductos();
+        return "Producto";
+    }
+    
+    public String editar(){
+        try {
+            ProductoJpaController controller = (ProductoJpaController) servletContext.getAttribute("productoJpaController");
+            controller.edit(producto);
+        } catch (Exception ex) {
+            Logger.getLogger(BeanProducto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        productos = listarProductos();
+        return "Producto";
+    }
+    
+    public String prepararEdicion(Integer id){
+        ProductoJpaController controller = (ProductoJpaController) servletContext.getAttribute("productoJpaController");
+        producto = controller.findProducto(id);
+        return "Producto";
+    }
+    
+    private List<Producto> listarProductos(){
+        ProductoJpaController controller = (ProductoJpaController) servletContext.getAttribute("productoJpaController");
+        return controller.findProductoEntities();
     }
 
     public Producto getProducto() {
