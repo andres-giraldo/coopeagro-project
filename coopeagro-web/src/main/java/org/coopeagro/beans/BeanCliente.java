f424f25676a19f144b4f0ca42f1bd7789f0ac023
YEISSON <YEISSON@YEISSON-PC>
2015-10-11 18:08:32 -0500

    Creacion, Modificacion, Eliminacion listos!!!!!

--- coopeagro-web/src/main/java/org/coopeagro/beans/BeanCliente.java
+++ coopeagro-web/src/main/java/org/coopeagro/beans/BeanCliente.java
@@ -8,12 +8,16 @@ package org.coopeagro.beans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
 import org.coopeagro.controladores.ClienteJpaController;
+import org.coopeagro.controladores.exceptions.NonexistentEntityException;
 import org.coopeagro.entidades.Cliente;
 import org.coopeagro.entidades.TiposDocumento;
 
@@ -32,7 +36,60 @@ public class BeanCliente {
      * Creates a new instance of BeanCliente
      */
     public BeanCliente() {
+        //ClienteJpaController controller = (ClienteJpaController) servletContext.getAttribute("clienteJpaController");
+        clientes = listarClientes();
+    }
+    
+    public void guardar(){
+        FacesMessage msg = null;
+        try {
+            ClienteJpaController controller = (ClienteJpaController) servletContext.getAttribute("clienteJpaController");
+            controller.create(cliente);
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "El registro fue insertado con éxito");
+        } catch (Exception ex) {
+            Logger.getLogger(BeanCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        clientes = listarClientes();
+    }
+    
+    public String eliminar(String id, TiposDocumento tipo){
+        try {
+            Cliente clienteEliminar = new Cliente();
+            clienteEliminar.getLlavePrimaria().setDocumento(id);
+            clienteEliminar.getLlavePrimaria().setTipoDocumento(tipo);
+            ClienteJpaController controller = (ClienteJpaController) servletContext.getAttribute("clienteJpaController");
+            controller.destroy(clienteEliminar.getLlavePrimaria());
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(BeanCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        clientes = listarClientes();
+        return "Cliente";
+    }
+    
+    public String editar(){
+        try {
+            ClienteJpaController controller = (ClienteJpaController) servletContext.getAttribute("clienteJpaController");
+            controller.edit(cliente);
+        } catch (Exception ex) {
+            Logger.getLogger(BeanCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        clientes = listarClientes();
+        return "Cliente";
+    }
+    
+    public String prepararEdicion(String id, TiposDocumento tipo){
+        Cliente clienteConsultar = new Cliente();
+        clienteConsultar.getLlavePrimaria().setDocumento(id);
+        clienteConsultar.getLlavePrimaria().setTipoDocumento(tipo);
         ClienteJpaController controller = (ClienteJpaController) servletContext.getAttribute("clienteJpaController");
+        cliente = controller.findCliente(clienteConsultar.getLlavePrimaria());
+        return "Cliente";
+    }
+    
+    private List<Cliente> listarClientes(){
+        ClienteJpaController controller = (ClienteJpaController) servletContext.getAttribute("clienteJpaController");
+        return controller.findClienteEntities();
     }
     
     public TiposDocumento[] getTiposDocumentoValues() {
--- coopeagro-web/src/main/java/org/coopeagro/beans/BeanEmpleado.java
+++ coopeagro-web/src/main/java/org/coopeagro/beans/BeanEmpleado.java
@@ -8,14 +8,20 @@ package org.coopeagro.beans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.inject.Named;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
 import org.coopeagro.controladores.EmpleadoJpaController;
+import org.coopeagro.controladores.UsuarioJpaController;
+import org.coopeagro.controladores.exceptions.NonexistentEntityException;
 import org.coopeagro.entidades.Empleado;
 import org.coopeagro.entidades.TiposDocumento;
+import org.coopeagro.entidades.Usuario;
 
 /**
  *
@@ -27,12 +33,68 @@ import org.coopeagro.entidades.TiposDocumento;
 public class BeanEmpleado {
     private final ServletContext servletContext = (ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext();
     Empleado empleado = new Empleado();
+    Usuario usuario = new Usuario();
     List<Empleado> empleados = new ArrayList<Empleado>();
     /**
      * Creates a new instance of BeanEmpleado
      */
     public BeanEmpleado() {
+        //EmpleadoJpaController controller = (EmpleadoJpaController) servletContext.getAttribute("empleadoJpaController");
+        empleados = listarEmpleados();
+    }
+    
+    public void guardar(){
+        FacesMessage msg = null;
+        try {
+            EmpleadoJpaController controller = (EmpleadoJpaController) servletContext.getAttribute("empleadoJpaController");
+            //UsuarioJpaController controllerUsu = (UsuarioJpaController) servletContext.getAttribute("usuarioJpaController");
+            //controllerUsu.create(usuario);
+            controller.create(empleado);
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "El registro fue insertado con éxito");
+        } catch (Exception ex) {
+            Logger.getLogger(BeanEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        empleados = listarEmpleados();
+    }
+    
+    public String eliminar(String id, TiposDocumento tipo){
+        try {
+            Empleado empleadoEliminar = new Empleado();
+            empleadoEliminar.getLlavePrimaria().setDocumento(id);
+            empleadoEliminar.getLlavePrimaria().setTipoDocumento(tipo);
+            EmpleadoJpaController controller = (EmpleadoJpaController) servletContext.getAttribute("empleadoJpaController");
+            controller.destroy(empleadoEliminar.getLlavePrimaria());
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(BeanEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        empleados = listarEmpleados();
+        return "Empleado";
+    }
+    
+    public String editar(){
+        try {
+            EmpleadoJpaController controller = (EmpleadoJpaController) servletContext.getAttribute("empleadoJpaController");
+            controller.edit(empleado);
+        } catch (Exception ex) {
+            Logger.getLogger(BeanEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        empleados = listarEmpleados();
+        return "Empleado";
+    }
+    
+    public String prepararEdicion(String id, TiposDocumento tipo){
+        Empleado empleadoConsultar = new Empleado();
+        empleadoConsultar.getLlavePrimaria().setDocumento(id);
+        empleadoConsultar.getLlavePrimaria().setTipoDocumento(tipo);
         EmpleadoJpaController controller = (EmpleadoJpaController) servletContext.getAttribute("empleadoJpaController");
+        empleado = controller.findEmpleado(empleadoConsultar.getLlavePrimaria());
+        return "Empleado";
+    }
+    
+    private List<Empleado> listarEmpleados(){
+        EmpleadoJpaController controller = (EmpleadoJpaController) servletContext.getAttribute("empleadoJpaController");
+        return controller.findEmpleadoEntities();
     }
     
     public TiposDocumento[] getTiposDocumentoValues() {
@@ -53,5 +115,13 @@ public class BeanEmpleado {
 
     public void setEmpleados(List<Empleado> empleados) {
         this.empleados = empleados;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 }
